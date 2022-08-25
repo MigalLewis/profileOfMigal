e5b3cc0a69c77f0ec6bb09c9bfbc580e0a8d1123
Migal <migallewis@gmail.com>
2022-08-25 18:33:23 +0200

    added angular fire

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
+  firebase: {
+    projectId: 'profile-of-migal',
+    appId: '1:604139082822:web:eb26aa94f00db0f2a56647',
+    storageBucket: 'profile-of-migal.appspot.com',
+    apiKey: 'AIzaSyCZ6eW9HwnPzmLQCcPWbIktyrL25Fr4pQ0',
+    authDomain: 'profile-of-migal.firebaseapp.com',
+    messagingSenderId: '604139082822',
+  },
   production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,14 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'profile-of-migal',
+    appId: '1:604139082822:web:eb26aa94f00db0f2a56647',
+    storageBucket: 'profile-of-migal.appspot.com',
+    apiKey: 'AIzaSyCZ6eW9HwnPzmLQCcPWbIktyrL25Fr4pQ0',
+    authDomain: 'profile-of-migal.firebaseapp.com',
+    messagingSenderId: '604139082822',
+  },
   production: false
 };
 
