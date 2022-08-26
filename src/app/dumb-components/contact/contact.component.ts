b777a6323ef53a957d8f8682618ba97c721e0eb1
Migal <migallewis@gmail.com>
2022-08-26 12:38:24 +0200

    added contact"

---
+++ src/app/dumb-components/contact/contact.component.ts
@@ -0,0 +1,22 @@
+import { Component, HostListener, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-contact',
+  templateUrl: './contact.component.html',
+  styleUrls: ['./contact.component.scss']
+})
+export class ContactComponent implements OnInit {
+  public innerWidth: any;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.innerWidth = window.innerWidth;
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.innerWidth = window.innerWidth;
+  }
+
+}
--- src/app/dumb-components/dumb-components.module.ts
+++ src/app/dumb-components/dumb-components.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
+import { ContactComponent } from './contact/contact.component';
 
 
 
 @NgModule({
   declarations: [
-    HeaderComponent
+    HeaderComponent,
+    ContactComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
-    HeaderComponent
+    HeaderComponent,
+    ContactComponent
   ]
 })
 export class DumbComponentsModule { }
