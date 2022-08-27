1da88bcf5d41f88e62aa195b6b8e760a7752152c
Migal <migallewis@gmail.com>
2022-08-27 12:13:54 +0200

    added progress bar

--- src/app/dumb-components/dumb-components.module.ts
+++ src/app/dumb-components/dumb-components.module.ts
@@ -2,20 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { ContactComponent } from './contact/contact.component';
+import { ProgressBarComponent } from './progress-bar/progress-bar.component';
 
 
 
 @NgModule({
   declarations: [
     HeaderComponent,
-    ContactComponent
+    ContactComponent,
+    ProgressBarComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
     HeaderComponent,
-    ContactComponent
+    ContactComponent,
+    ProgressBarComponent
   ]
 })
 export class DumbComponentsModule { }
