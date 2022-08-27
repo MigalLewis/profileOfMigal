d0c090393aea3b034e6119675de2ec5a52c2410c
Migal <migallewis@gmail.com>
2022-08-27 10:15:53 +0200

    added about me page

--- src/app/pages/navigating/home-page/home-page.module.ts
+++ src/app/pages/navigating/home-page/home-page.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 import { LandingComponent } from './landing/landing.component';
 import { HomePageComponent } from './home-page.component';
 import { DumbComponentsModule } from 'src/app/dumb-components/dumb-components.module';
+import { AboutMeComponent } from './about-me/about-me.component';
 
 
 
 @NgModule({
   declarations: [
     LandingComponent,
-    HomePageComponent
+    HomePageComponent,
+    AboutMeComponent
   ],
   imports: [
     CommonModule,
