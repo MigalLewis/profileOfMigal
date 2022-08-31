e101f93fe000fe88edd16e20dc664a38ecbbacf4
Migal <migallewis@gmail.com>
2022-08-31 15:03:50 +0200

    added footsteps for mobile

--- src/app/pages/navigating/home-page/my-experience/my-experience.component.ts
+++ src/app/pages/navigating/home-page/my-experience/my-experience.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { Company, Experience } from 'src/app/models/company.model';
 import { Footstep } from 'src/app/models/footstep.model';
 import { ExperienceService } from 'src/app/services/experience.service';
@@ -9,14 +9,49 @@ import { ExperienceService } from 'src/app/services/experience.service';
   styleUrls: ['./my-experience.component.scss']
 })
 export class MyExperienceComponent implements OnInit {
-
+  public innerWidth: any;
   footsteps: Footstep[];
   experience: Experience[];
   selectedExperience: Experience | undefined;
 
   constructor(private experienceService: ExperienceService) { 
-    
-    this.footsteps = [
+    this.innerWidth = window.innerWidth;
+    if(this.innerWidth>600) {
+      this.footsteps = this.getWebFootsteps();
+    } else {
+      this.footsteps = this.getMobileFootsteps();
+    }
+    this.footsteps = this.setStartEnd(0.5, 3.5,0.5,this.footsteps);
+    this.experience = [];
+    this.selectedExperience = undefined;
+  }
+
+  ngOnInit(): void {
+    this.experienceService.getExperience().subscribe((data:Experience[]) => {
+      this.experience = data;
+    });
+  }
+
+  setStartEnd(startingPoint: number, endingPoint: number, increment: number, footsteps: Footstep[]):  Footstep[] {
+    return footsteps.map(f => {
+      f.startEnd=startingPoint+'s, '+endingPoint+'s';
+      startingPoint+=increment;
+      endingPoint+=increment;
+      return f;
+    });
+  }
+
+  open(name: string) {
+    this.selectedExperience = this.experience.find(e => {
+      if(e.company.name === name) {
+        return e;
+      }
+      return undefined;
+    });
+  }
+
+  getWebFootsteps(): Footstep[] {
+    return [
       {x: '52%', y:'4%'},
       {x: '53%', y:'9%'},
       {x: '56%', y:'8%'},
@@ -50,35 +85,32 @@ export class MyExperienceComponent implements OnInit {
       {x: '37%', y:'63%', transform: 'rotate(26deg)'},
       {x: '39%', y:'68%', transform: 'rotate(26deg)'},
     ];
-    this.footsteps = this.setStartEnd(0.5, 3.5,0.5,this.footsteps);
-    this.experience = [];
-    this.selectedExperience = undefined;
   }
 
-  ngOnInit(): void {
-    this.experienceService.getExperience().subscribe((data:Experience[]) => {
-      this.experience = data;
-    });
+  getMobileFootsteps(): Footstep[] {
+    return [
+      {x: '38%', y:'2%', transform: 'rotate(296deg)'},
+      {x: '59%', y:'9%', transform: 'rotate(334deg)'},
+      {x: '56%', y:'12%', transform: 'rotate(334deg)'},
+
+      {x: '52%', y:'16%', transform: 'rotate(198deg)'},
+      {x: '42%', y:'17%', transform: 'rotate(210deg)'},
+      {x: '32%', y:'16%', transform: 'rotate(225deg)'},
+
+      {x: '52%', y:'22%', transform: 'rotate(351deg)'},
+      {x: '52%', y:'25%', transform: 'rotate(344deg)'},
+      {x: '65%', y:'25%', transform: 'rotate(330deg)'},
+
+      {x: '57%', y:'32%', transform: 'rotate(230deg)'},
+      {x: '51%', y:'34%', transform: 'rotate(234deg)'},
+      {x: '41%', y:'38%', transform: 'rotate(320deg)'},
+      {x: '36%', y:'40%', transform: 'rotate(320deg)'},
+    ];
   }
 
-  setStartEnd(startingPoint: number, endingPoint: number, increment: number, footsteps: Footstep[]):  Footstep[] {
-    return footsteps.map(f => {
-      f.startEnd=startingPoint+'s, '+endingPoint+'s';
-      startingPoint+=increment;
-      endingPoint+=increment;
-      return f;
-    });
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.innerWidth = window.innerWidth;
   }
-
-  open(name: string) {
-    this.selectedExperience = this.experience.find(e => {
-      if(e.company.name === name) {
-        return e;
-      }
-      return undefined;
-    });
-  }
-
-  
 
 }
